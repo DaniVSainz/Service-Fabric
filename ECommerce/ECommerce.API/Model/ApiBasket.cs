8b831ea5634215b2b9b21e077f8e4d1a1e070783
danivsainz <danivsainz@gmail.com>
2018-08-27 23:55:14 -0700

    add user baskets

---
+++ ECommerce/ECommerce.API/Model/ApiBasket.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.API.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.ProductCatalog.Model
+{
+    public class ApiBasket
+    {
+        [JsonProperty("userId")]
+        public string UserId { get; set; }
+
+        [JsonProperty("item")]
+        public ApiBasketItem[] Items { get; set; }
+
+    }
+}
